85823941b39c5c99be77125ec8015ed2e5ac5a28
Belen Tracchia <48957384+BeluTrac@users.noreply.github.com>
2022-01-23 23:52:56 +0100

    user location

--- app/src/main/java/com/belutrac/earthquakemonitor/map/MapsActivity.kt
+++ app/src/main/java/com/belutrac/earthquakemonitor/map/MapsActivity.kt
@@ -1,8 +1,15 @@
 package com.belutrac.earthquakemonitor.map
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import com.belutrac.earthquakemonitor.Earthquake
 import com.belutrac.earthquakemonitor.R
 
@@ -13,12 +20,18 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.belutrac.earthquakemonitor.databinding.ActivityMapsBinding
+import com.google.android.gms.location.LocationServices
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
+private const val LOCATION_PERMISSION_REQUEST_CODE = 2000
+private const val DEFAULT_MAP_SCALE = 5.0f
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private lateinit var earthquake: ArrayList<Earthquake>
+    private val userLocation = Location("")
+    private lateinit var myLocationButton: FloatingActionButton
 
     companion object{
         const val EARTHQUAKE_LIST_KEY = "earthquake"
@@ -31,14 +44,76 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(binding.root)
 
         earthquake = intent?.getParcelableArrayListExtra<Earthquake>(EARTHQUAKE_LIST_KEY) as ArrayList<Earthquake>
+
+        checkLocationPermission()
+    }
+
+    private fun checkLocationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                getUserLocation()
+            } else {
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            }
+        } else {
+            getUserLocation()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getUserLocation() {
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            if (location != null) {
+                userLocation.latitude = location.latitude
+                userLocation.longitude = location.longitude
+                setupMap()
+            }
+        }
+    }
+
+    private fun setupMap() {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                getUserLocation()
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                showLocationPermissionRationaleDialog()
+            } else {
+                finish()
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun showLocationPermissionRationaleDialog() {
+        val dialog = AlertDialog.Builder(this)
+            .setTitle(R.string.need_location_permission_dialog_title)
+            .setMessage(R.string.need_location_permission_dialog_message)
+            .setPositiveButton(android.R.string.ok) { _, _ ->
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    LOCATION_PERMISSION_REQUEST_CODE)
+            }.setNegativeButton(R.string.no) { _, _ ->
+                finish()
+            }
+        dialog.show()
+    }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+
+        val userLatLng = LatLng(userLocation.latitude, userLocation.longitude)
+        val userMarker = MarkerOptions().position(userLatLng)
+        val myLocationButton = binding.myLocationButton
+        mMap.addMarker(userMarker)
 
         for (eq in earthquake){
             val place = LatLng(eq.latitude,eq.longitude)
@@ -47,5 +122,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 .title(eq.place).snippet("Magnitude: ${eq.magnitude} Richter"))
         }
 
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, DEFAULT_MAP_SCALE))
+
+        myLocationButton.setOnClickListener {
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, DEFAULT_MAP_SCALE))
+        }
     }
 }
