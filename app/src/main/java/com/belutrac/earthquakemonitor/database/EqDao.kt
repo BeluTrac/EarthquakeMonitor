6141a1f913052802a2848db732638915c8537d61
Belen Tracchia <48957384+BeluTrac@users.noreply.github.com>
2022-01-23 02:37:57 +0100

    Database files created.

---
+++ app/src/main/java/com/belutrac/earthquakemonitor/database/EqDao.kt
@@ -0,0 +1,22 @@
+package com.belutrac.earthquakemonitor.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.belutrac.earthquakemonitor.Earthquake
+
+@Dao //Para cada entity debo tener una interfaz Dao
+
+interface EqDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(eqList : MutableList<Earthquake>)
+
+    @Query("SELECT * FROM earthquakes")
+    fun getEarthquake(): MutableList<Earthquake>
+
+    @Query("SELECT * FROM earthquakes order by magnitude ASC")
+    fun getEarthquakeByMagnitude(): MutableList<Earthquake>
+}
